8cdca15c18902912c6a2db22593eab215f482f34
kodai <kodai030513@gmail.com>
2024-07-23 12:40:03 +0900

    不要なファイルとコントローラークラスを作成

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -5,6 +5,11 @@ import { createRoot } from 'react-dom/client';
 import { createInertiaApp } from '@inertiajs/react';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
 createInertiaApp({
