e5714a53213a67709043f8227ace0f416fb1a0e7
kodai <kodai030513@gmail.com>
2024-08-14 02:29:26 +0900

    本登録処理の修正

--- app/UseCases/RegisterUser/StoreAction.php
+++ app/UseCases/RegisterUser/StoreAction.php
@@ -4,16 +4,24 @@ declare(strict_types=1);
 
 namespace App\Usecases\RegisterUser;
 
+use App\Models\Profile;
+use App\Models\SettingMailNotice;
 use App\Models\TempUser;
 use App\Models\User;
+use App\Models\WebNotice;
 use App\Usecases\RegisterUser\Exceptions\RegisteredUserException;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
 class StoreAction
 {
     public function __invoke(
-        Request $request,
-        TempUser $tempUserModel,
-        User $userModel
+        Request           $request,
+        TempUser          $tempUserModel,
+        User              $userModel,
+        WebNotice         $webNoticeModel,
+        SettingMailNotice $settingMailNoticeModel,
+        Profile           $profileModel
     ): void
     {
         $targetTempUser = $tempUserModel->where('token', '=', $request->token)->first()->toArray();
@@ -26,5 +34,11 @@ class StoreAction
         }
 
         $userModel->create($targetTempUser);
+
+        $userId = DB::getPdo()->lastInsertId();
+
+        $webNoticeModel->create(['received_user_id' => $userId]);
+        $settingMailNoticeModel->create(['user_id' => $userId]);
+        $profileModel->create(['user_id' => $userId]);
     }
 }
